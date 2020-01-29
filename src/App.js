3fa21972ca44cb79128e9f634870d54494b807a0
Torell <Sallie.Torell@us.sogeti.com>
2020-01-29 14:22:36 -0500

    connect employee api, add bootstrap, display employees in app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3005/getSogetiEmployees')
+    .then(employees => employees.json())
+    .then(_employees => {
+      console.log(_employees)
+      this.setState({
+        employees: _employees
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className="App container">
+        <h1>Sogeti Employee Directory</h1>
+        <div className="row">
+          { this.state.employees.map((employee, index) => {
+            return (
+              <div className="card mb-4 p-3 directory-entry">
+                <img src={employee.image} class="directory-image" alt="..."/>
+                <div class="card-body">
+                    <h5 className="card-title">{employee.name}</h5>
+                    <p class="card-text">{employee.location}</p>
+                    <p className="card-text2">{employee.phone}</p>
+                    <a href="#" class="btn btn-primary">See more</a>
+                </div>
+              </div>
+
+              /* <div className="media col-12 mb-4 p-3 directory-entry">
+                <img src={employee.image} className="mr-3 directory-image" alt="..."/>
+                  <div className="media-body">
+                    <h5 className="mt-0">{employee.name}</h5>
+                    {employee.location}
+                    {employee.phone}
+                  </div>
+              </div> */
+            )
+          })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
